6424abb2c377cd511e2e3629233abd431421644e
dhana <tkdhanasekar@gmail.com>
2019-01-19 17:15:05 +0530

    first commit

--- git.sh
+++ git.sh
@@ -8,6 +8,9 @@ git config --global user.email "tkdhanasekar@gmail.com"
 git config --global user.name "dhana"
 git commit -m "first commit"
 git status
+git add first.py
+git commit -m "msg" first.py
 git pull dhana master
-git remote -v
+git push dhana master
+git log
 
