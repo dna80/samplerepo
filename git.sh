aeffb7545f7bd8a9b4708831232fbb53b10f6d56
dhana <tkdhanasekar@gmail.com>
2019-01-19 17:24:56 +0530

    first commit

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 mkdir demorepo
 git init
@@ -7,6 +7,7 @@ git commit -m "first commit"
 git config --global user.email "tkdhanasekar@gmail.com"
 git config --global user.name "dhana"
 git commit -m "first commit"
+git remote add dhana https://github.com/tkdhanasekar/samplerepo
 git status
 git add first.py
 git commit -m "msg" first.py
